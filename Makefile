1b33633b9002cc983664dae99541997f9aa7ac0f
tobiascr <tobiascrw@gmail.com>
2020-12-08 04:45:41 +0100

    modified:   Makefile 	modified:   chess.cpp

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 FLAGS=-O3
 
-engine_objects=chess.o
+engine_objects=chess.o game_state.o
 
 chess: $(engine_objects)
 	g++ $(FLAGS) $(engine_objects) -o chess
 
 chess.o: chess.cpp
 	g++ -c $(FLAGS) chess.cpp
+
+game_state.o: game_state.cpp
+	g++ -c $(FLAGS) game_state.cpp
 
 .PHONY: clean
 clean:
--- chess.cpp
+++ chess.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <vector>
+#include "move.h"
+#include "game_state.h"
 
 int main()
 {
+    std::cout << "Chess" << std::endl;
+
+    Move move;
+
+    move.type = MoveType::queenside_castling;
+    move.from_square = 1;
+    move.to_square = 1;
+
+    std::cout << "type: " << move.type << std::endl;
+    std::cout << "from: "<< move.from_square << std::endl;
+    std::cout << "to: "<< move.to_square << std::endl;
+
+    std::vector<Move> move_list;
+
+    move_list.push_back(move);
+
+    GameState game_state;
+
+    std::cout << -game_state.player_in_turn << std::endl;
+
+    game_state.make_move(move);
+    game_state.undo_move(move);
+
+    game_state.print_board();
+
+    BoardItem piece;
+    piece.type = BoardItemType::rook;
+    piece.color = Color::black;
+    game_state.board[25] = piece;
+
+    std::cout << "type: " << game_state.board[25].type << std::endl;
+    std::cout << "color: " << game_state.board[25].color << std::endl;
+
     return 0;
 }
