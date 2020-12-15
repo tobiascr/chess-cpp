3556c0f2dae4e877b5851e0ef57d21f1337a9fa9
tobiascr <tobiascrw@gmail.com>
2020-12-15 07:51:44 +0100

    Made a position value function.

--- chess.cpp
+++ chess.cpp
@@ -74,10 +74,46 @@ void print_board(GameState& game_state)
     std::cout << std::endl;
 }
 
+int position_value(GameState& game_state)
+/* Give a positive value for positions favorable for the player in turn and negative
+if favorable for the other player.*/
+{
+    int value = 0;
+
+    for(int r=20; r<=90; r+=10)
+    {
+        for(int c=1; c<=8; c++)
+        {
+            switch(game_state.board[r+c].type)
+            {
+                case BoardItem::king:
+                    value += 1000 * game_state.board[r+c].color;
+                    break;
+                case BoardItem::queen:
+                    value += 9 * game_state.board[r+c].color;
+                    break;
+                case BoardItem::rook:
+                    value += 5 * game_state.board[r+c].color;
+                    break;
+                case BoardItem::bishop:
+                    value += 3 * game_state.board[r+c].color;
+                    break;
+                case BoardItem::knight:
+                    value += 3 * game_state.board[r+c].color;
+                    break;
+                case BoardItem::pawn:
+                    value += 1 * game_state.board[r+c].color;
+                    break;
+            }
+        }
+    }
+    return value * game_state.player_in_turn;
+}
+
 int main()
 {
     GameState game_state;
-    game_state.load_FEN_string("8/3p4/8/2P5/8/8/8/8 b - - 0 1");
+    game_state.load_FEN_string("rnbk2n1/ppppbppr/7p/4p1B1/4P3/3P4/PPP2PPP/RN1QKBNR w KQq - 0 1");
 
     MoveGenerator generator;
     std::vector<Move>& move_list =
@@ -96,6 +132,8 @@ int main()
     }
     std::cout << "Starting position:" << std::endl;
     print_board(game_state);
+
+    std::cout << "Position value: " << position_value(game_state) << std::endl;
 
     return 0;
 }
