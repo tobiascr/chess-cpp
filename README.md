d6cae1ffc828e72b1342512a25c0d344dc01b90a
tobiascr <tobiascrw@gmail.com>
2020-12-20 11:37:50 +0100

    modified:   README.md 	modified:   test.py

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # chess-cpp
+
+A chess engine made with C++. The user can type in a position in FEN format and get a
+best move returned.
+
+### Installation on Linux
+
+Download the repository as a zip-file. Uncompress it and go to
+its folder in the terminal.
+
+To compile the program run
+
+    make
+
+You can then run it with
+
+    ./chess
--- test.py
+++ test.py
@@ -22,8 +22,11 @@ test_FEN("r4r2/1pp4k/p3P2p/2Pp1p2/bP5Q/P3q3/1B1n4/K6R w - - 1 33")
 test_FEN("2bqkbnr/5ppp/pB1pp3/p2N4/4P3/P4N2/2P1QPPP/1R4K1 b k - 0 1")
 test_FEN("3B1bnr/1Q3ppp/3ppk2/p7/4P3/P4N2/2q2PPP/1R4K1 b - - 0 1")
 
-# Lichess puzzle, best move: b7a6.
+# Lichess puzzle, best move: b7a6
 test_FEN("r4rk1/1b2q1p1/2n1p2p/1Qb1Pp2/3p1P2/P4N1N/2PB2PP/R4R1K b - - 2 21")
+
+# Lichess puzzle, best move: e8e1
+test_FEN("k3r2r/p2Q4/2p4p/6p1/1R3pq1/2P5/PP3P1P/3R3K b - - 13 29")
 
 print()
 print("Total time:", total_time)
